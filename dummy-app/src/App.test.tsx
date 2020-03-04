0349fe98b3cb9bfd347f59df9384c62250db96c2
Samuel Gassauer <samuel.gassauer@student.kit.edu>
2020-03-04 10:35:05 +0100

    test added

--- dummy-app/src/App.test.tsx
+++ dummy-app/src/App.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, getByRole } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { getAllByText, getByRole } = render(<App />);
+  expect(getAllByText('About Me'))
 });
