860e8ead069ee32b7034d69292448428254a3255
Samuel Gassauer <samuel.gassauer@student.kit.edu>
2020-03-05 03:10:17 +0100

    wip

--- dummy-app/src/test/App.test.tsx
+++ dummy-app/src/test/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, getByRole } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 test('renders learn react link', () => {
   const { getAllByText, getByRole } = render(<App />);
